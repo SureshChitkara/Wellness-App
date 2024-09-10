3d26e34bbd736ac836c7fb8eb8c33a685b01db11
SureshChitkara <156342133+SureshChitkara@users.noreply.github.com>
2024-09-10 14:52:50 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomeScreen from './components/HomeScreen';
-import NavBar from './components/NavBar';
-import JourneyPage from './JourneyPage'; // Import the new page component
-import './App.css';
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<HomeScreen />} />
-                    <Route path="/journey" element={<JourneyPage />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+.App {
+  text-align: center;
 }
 
-export default App;
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.App-header {
+  /* Replace the existing background color with your gradient background */
+
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
