a5ec73ee1b86dbe9a430c7049844aa779e9ef43e
SureshChitkara <156342133+SureshChitkara@users.noreply.github.com>
2024-09-24 14:45:25 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomeScreen from './components/HomeScreen';
-import NavBar from './components/NavBar';
-import JourneyPage from './components/JourneyPage';  // Import the JourneyPage component
 import About from './components/About';  // Example About component
 import Contact from './components/Contact';  // Example Contact component
-import MeditationLibrary from './components/MeditationLibrary'; // Import the MeditationLibrary component
-import VirtualTherapy from './components/VirtualTherapy'; // Import the VirtualTherapy component
-import MoodTracking from './components/MoodTracking'; // Import the VirtualTherapy component
-
-import './App.css';
+import NavBar from './components/NavBar';
+import JourneyPage from './components/JourneyPage';
+import MeditationLibrary from './components/MeditationLibrary';
+import VirtualTherapy from './components/VirtualTherapy'
+import MoodTracking from './components/MoodTracking';
+import { MoodProvider } from './context/MoodContext'; // Import the MoodProvider
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <NavBar />
-                <Routes>
-                    <Route path="/" element={<HomeScreen />} />
-                    <Route path="/journey" element={<JourneyPage />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/meditation-library" element={<MeditationLibrary />} />
-                    <Route path="/virtual-therapy" element={<VirtualTherapy />} /> {/* Add this line */}
-                    <Route path="/mood-tracking" element={<MoodTracking />} /> {/* Add this route */}
-                </Routes>
-            </div>
-        </Router>
+        <MoodProvider>
+            <Router>
+                <div className="App">
+                    <NavBar />
+                    <Routes>
+                        <Route path="/" element={<HomeScreen />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/journey" element={<JourneyPage />} />
+                        <Route path="/meditation-library" element={<MeditationLibrary />} />
+                        <Route path="/virtual-therapy" element={<VirtualTherapy />} />
+                        <Route path="/mood-tracking" element={<MoodTracking />} />
+                    </Routes>
+                </div>
+            </Router>
+        </MoodProvider>
     );
 }
 
